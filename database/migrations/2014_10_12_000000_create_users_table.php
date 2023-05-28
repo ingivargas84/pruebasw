8358383a8a7f54fcdc12d0a89c8a5e3b93407dd8
Ing Iver Vargas <ing.ivargas84@gmail.com>
2023-05-27 22:14:34 -0600

    Se creo la funcionalidad de autenticación

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('tipo_usuario')->default(0); //0-suscriptor 1-administrador
             $table->rememberToken();
             $table->timestamps();
         });
